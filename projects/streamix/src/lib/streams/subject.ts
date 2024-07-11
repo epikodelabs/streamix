a8e2f052ab6e95332fe7b4455d956a56dafecf05
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-07-11 12:19:15 +0300

    refactor: simplify Subject to a plain emission queue

--- projects/streamix/src/lib/streams/subject.ts
+++ projects/streamix/src/lib/streams/subject.ts
@@ -3,58 +3,48 @@ import { AbstractStream } from '../abstractions/stream';
 import { Promisified } from '../utils/promisified';
 
 export class Subject extends AbstractStream {
-  protected emissionQueue: Promisified<Emission>[] = [];
-  protected hasNewValue = new Promisified<boolean>(false);
-
-  constructor() {
-    super();
-  }
+  private emissionQueue: Emission[] = [];
+  private emissionAvailable = new Promisified<boolean>(false);
 
   async run(): Promise<void> {
     try {
       while (!this.isStopped.value && !this.isUnsubscribed.value) {
-        await Promise.race([this.isUnsubscribed.promise, this.hasNewValue.promise]);
+        await Promise.race([this.isUnsubscribed.promise, this.emissionAvailable.promise]);
 
-        if(!this.isStopped.value && !this.isUnsubscribed.value) {
-          this.hasNewValue.reset();
-        }
+        if (this.isStopped.value || this.isUnsubscribed.value) break;
 
-        while (this.emissionQueue.length > 0 && !this.isStopped.value && !this.isUnsubscribed.value) {
+        this.emissionAvailable.reset();
+
+        while (this.emissionQueue.length > 0) {
           if (this.isCancelled.value || this.isUnsubscribed.value) {
-            this.emissionQueue.forEach(emission => emission.value.isCancelled = true);
+            this.emissionQueue = []; // Clear the queue
             this.isStopped.resolve(true);
             break;
-          } else {
-            const promisified = this.emissionQueue.shift()!;
-            const emission: Emission = promisified.value;
-            await super.emit(emission);
-            promisified.resolve(emission);
+          }
 
-            if (this.isStopRequested.value && this.emissionQueue.length == 0) {
-              this.isStopped.resolve(true);
-              break;
-            }
+          const emission = this.emissionQueue.shift()!;
+          await super.emit(emission);
+
+          if (this.isStopRequested.value && this.emissionQueue.length === 0) {
+            this.isStopped.resolve(true);
+            break;
           }
         }
       }
-    }
-    catch (error: any) {
+    } catch (error: any) {
       console.warn(`Error in Subject ${this.constructor.name} run:`, error);
     }
   }
 
-  next(value: any): Promise<void> {
+  next(value: any): void {
     if (this.isStopped.value) {
       console.warn('Cannot push value to a stopped Subject.');
-      return Promise.resolve();
+      return;
     }
-    if (!this.isStopRequested.value && !this.isCancelled.value) {
-      const emission = new Promisified<Emission>({value});
-      this.emissionQueue.push(emission);
-      this.hasNewValue.resolve(true);
 
-      return emission.promise.then(() => Promise.resolve());
+    if (!this.isStopRequested.value && !this.isCancelled.value) {
+      this.emissionQueue.push({ value });
+      this.emissionAvailable.resolve(true);
     }
-    return this.isStopped.promise.then(() => Promise.resolve());
   }
 }
