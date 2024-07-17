68de150fb3a6d498d5de4adb51d69c0c6ebb263d
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-07-17 11:00:21 +0300

    refactor(concatMap): queue inner streams through split sink processing

--- projects/streamix/src/lib/operators/concatMap.ts
+++ projects/streamix/src/lib/operators/concatMap.ts
@@ -1,11 +1,13 @@
-import { AbstractOperator, AbstractStream, Emission } from '../abstractions';
+import { AbstractOperator, AbstractStream, Emission, StreamSink } from '../abstractions';
 import { Promisified } from '../utils/promisified';
 
 export class ConcatMapOperator extends AbstractOperator {
   private readonly project: (value: any) => AbstractStream;
   private queue: Emission[] = [];
   private isProcessing = new Promisified<boolean>(false);
-  private isFullfilled = new Promisified<boolean>(false);
+
+  private left!: StreamSink;
+  private right!: StreamSink;
 
   constructor(project: (value: any) => AbstractStream) {
     super();
@@ -21,65 +23,55 @@ export class ConcatMapOperator extends AbstractOperator {
     this.queue.push(emission);
 
     if (!this.isProcessing.value) {
-      try {
-        await this.processQueue(stream);
-      } catch (error) {
-        return Promise.reject(error); // Reject the outer promise
-      }
-      return emission;
+      this.isProcessing.resolve(true);
+      await this.processQueue(stream);
+      this.isProcessing.reset();
     }
 
-    // Return a new stream that will be resolved when this emission is processed
-    return new Promise<Emission>((resolve, reject) => {
-      if (!this.isFullfilled.value) {
-        this.isFullfilled.promise.then(() => {
-          this.isFullfilled.reset();
-          this.processQueue(stream)
-            .then(() => resolve(emission))
-            .catch((error) => reject(error));
-        }).catch((error) => reject(error)); // Propagate errors from isFulfilled.promise
-      }
-    });
+    emission.isPhantom = true;
+    return emission;
   }
 
-  private async processQueue(stream: AbstractStream) {
-    this.isProcessing.resolve(true);
-
+  private async processQueue(stream: AbstractStream): Promise<void> {
     while (this.queue.length > 0 && !stream.isCancelled.value) {
       const emission = this.queue.shift()!;
       const innerStream = this.project(emission.value);
 
+      let streamSink = stream as StreamSink;
+      if (!(streamSink instanceof StreamSink)) {
+        streamSink = new StreamSink(streamSink);
+      }
+      if (this.left === undefined) {
+        const [left, right] = streamSink.split(this, streamSink);
+        this.left = left;
+        this.right = right;
+      }
+
       try {
-        await new Promise<void>((resolve, reject) => {
-          const subscription = innerStream.subscribe((value) => {
-            if (this.next) {
-              this.next.process({ value }, stream);
-            }
-          });
-
-          innerStream.isFailed.promise.then((error) => {
-            emission.error = error;
-            emission.isFailed = true;
-            subscription.unsubscribe();
-            reject(error);
-          })
-
-          innerStream.isStopped.promise.then(() => {
-            resolve();
-          });
-        });
+        await this.processInnerStream(innerStream, stream);
       } catch (error) {
         emission.error = error;
         emission.isFailed = true;
       }
-
-      if(emission.isFailed) {
-        throw emission.error;
-      }
     }
+  }
 
-    this.isFullfilled.resolve(true);
-    this.isProcessing.reset();
+  private async processInnerStream(innerStream: AbstractStream, stream: AbstractStream): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      const subscription = innerStream.subscribe(async (value) => {
+        await this.right.emit({ value }).catch(reject);
+      });
+
+      innerStream.isFailed.promise.then((error) => {
+        subscription.unsubscribe();
+        reject(error);
+      });
+
+      innerStream.isStopped.promise.then(() => {
+        subscription.unsubscribe();
+        resolve();
+      }).catch(reject);
+    });
   }
 }
 
