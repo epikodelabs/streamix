f69102674be137432af7d40083757c67044e3451
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-07-25 09:04:28 +0300

    chore: delete projects/streamix/src/lib/hooks/noop.ts

--- projects/streamix/src/lib/hooks/noop.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractStream } from '../abstractions';
-
-export class NoopHook {
-  async process(stream: AbstractStream, params?: any): Promise<void> {
-    return Promise.resolve();
-  }
-}
