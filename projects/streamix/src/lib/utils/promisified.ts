81e1feef5ebbefb39b9241ed79fab8ac8d5d0b65
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-07-31 18:31:07 +0300

    refactor: update promisified.ts

--- projects/streamix/src/lib/utils/promisified.ts
+++ projects/streamix/src/lib/utils/promisified.ts
@@ -57,3 +57,69 @@ export class Promisified<T> {
     return Promise.race(promises.map(p => p.promise));
   }
 }
+
+function promisified<T>(initialValue: T) {
+  let _value = initialValue;
+  let _default = initialValue;
+  let _resolve!: (value: T) => void;
+  let _reject!: (reason?: any) => void;
+
+  let _promise = new Promise<T>((resolve, reject) => {
+    _resolve = resolve;
+    _reject = reject;
+  });
+
+  function innerFunction() {
+    return _value;
+  }
+
+  innerFunction.resolve = function (value: T): void {
+    if (_promise.then === undefined) {
+      throw new Error('Promise already settled');
+    }
+    _value = value;
+    _resolve(value);
+  };
+
+  innerFunction.reject = function (reason?: any): void {
+    if (_promise.then === undefined) {
+      throw new Error('Promise already settled');
+    }
+    _reject(reason);
+  };
+
+  Object.defineProperty(innerFunction, 'value', {
+    get: function () {
+      return _value;
+    }
+  });
+
+  Object.defineProperty(innerFunction, 'promise', {
+    get: function () {
+      return _promise;
+    }
+  });
+
+  innerFunction.reset = function () {
+    _promise = new Promise<T>((resolve, reject) => {
+      _resolve = resolve;
+      _reject = reject;
+      _value = _default;
+    });
+  };
+
+  innerFunction.then = function <U = void>(callback: (value?: T) => U | PromiseLike<U>): Promise<U> {
+    return _promise.then(callback);
+  };
+
+  innerFunction.all = function (promises: Array<ReturnType<typeof createPromisified<any>>>): Promise<any[]> {
+    return Promise.all(promises.map(p => p.promise));
+  };
+
+  innerFunction.race = function (promises: Array<ReturnType<typeof createPromisified<any>>>): Promise<any> {
+    return Promise.race(promises.map(p => p.promise));
+  };
+
+  return innerFunction;
+}
+
