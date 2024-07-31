bcd0f5dde38b83b222861d6b0c83f6f48d27dabe
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-07-31 18:43:08 +0300

    refactor: update counter.ts

--- projects/streamix/src/lib/utils/counter.ts
+++ projects/streamix/src/lib/utils/counter.ts
@@ -53,3 +53,56 @@ export class PromisifiedCounter {
     }
   }
 }
+
+function promisifiedCounter(initialValue: number = 0) {
+  let count = initialValue;
+  let listener: (() => void) | null = null;
+  let unsubscribeMethod: (() => void) | null = null;
+
+  async function innerFunction(): Promise<number> {
+    return count;
+  }
+
+  innerFunction.increment = async function (): Promise<void> {
+    count++;
+  };
+
+  innerFunction.decrement = async function (): Promise<void> {
+    count--;
+    if (count <= 0) {
+      notifyListener();
+    }
+  };
+
+  innerFunction.getCount = async function (): Promise<number> {
+    return count;
+  };
+
+  innerFunction.subscribe = function (newListener: () => void): () => void {
+    if (listener && unsubscribeMethod) {
+      return unsubscribeMethod;
+    }
+
+    listener = newListener;
+    unsubscribeMethod = () => {
+      listener = null;
+    };
+
+    return unsubscribeMethod;
+  };
+
+  innerFunction.unsubscribe = function (): void {
+    if (unsubscribeMethod) {
+      unsubscribeMethod();
+      unsubscribeMethod = null;
+    }
+  };
+
+  function notifyListener() {
+    if (listener) {
+      listener();
+    }
+  }
+
+  return innerFunction;
+}
