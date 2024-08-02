dca2ca04dd73660360bd52ef52433e0d2818d18a
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-08-02 13:34:59 +0300

    refactor: revert promisified

--- projects/streamix/src/lib/utils/promisified.ts
+++ projects/streamix/src/lib/utils/promisified.ts
@@ -1,90 +1,48 @@
 export function promisified<T>(initialValue: T) {
-  let _value: T = initialValue;
-  let _resolve: ((value: T) => void) | undefined;
-  let _reject: ((reason?: any) => void) | undefined;
-  let _promise: Promise<T> | undefined;
+  let _value = initialValue;
+  let _default = initialValue;
+  let _resolve!: (value: T) => void;
+  let _reject!: (reason?: any) => void;
 
-  const resolve = (value: T) => {
-    if (!_promise) {
-      createPromise(); // Ensure the promise is instantiated
-    }
-    if (_resolve) {
-      _value = value;
-      _resolve(value);
-      _resolve = undefined;
-      _reject = undefined;
-    } else {
+  let _promise = new Promise<T>((resolve, reject) => {
+    _resolve = resolve;
+    _reject = reject;
+  });
+
+  function innerFunction() {
+    return _value;
+  }
+
+  innerFunction.resolve = function (value: T): void {
+    if (_promise.then === undefined) {
       throw new Error('Promise already settled');
     }
+    _value = value;
+    _resolve(value);
   };
 
-  const reject = (reason?: any) => {
-    if (!_promise) {
-      createPromise(); // Ensure the promise is instantiated
-    }
-    if (_reject) {
-      _reject(reason);
-      _resolve = undefined;
-      _reject = undefined;
-    } else {
+  innerFunction.reject = function (reason?: any): void {
+    if (_promise.then === undefined) {
       throw new Error('Promise already settled');
     }
+    _reject(reason);
   };
 
-  const reset = () => {
-    _value = initialValue;
-    _promise = undefined; // Reset the promise
-  };
+  innerFunction.promise = _promise;
 
-  const createPromise = () => {
+  innerFunction.reset = function () {
     _promise = new Promise<T>((resolve, reject) => {
       _resolve = resolve;
       _reject = reject;
+      _value = _default;
     });
   };
 
-  const fn = function () {
-    return _value;
-  } as {
-    (): T;
-    promise: Promise<T>;
-    resolve: (value: T) => void;
-    reject: (reason?: any) => void;
-    reset: () => void;
-    then: (onFulfilled: (value: T) => void, onRejected?: (reason: any) => void) => Promise<void>;
+  innerFunction.then = function <U = void>(callback: (value?: T) => U | PromiseLike<U>): Promise<U> {
+    return _promise.then(callback);
   };
 
-  Object.defineProperties(fn, {
-    promise: {
-      get() {
-        if (!_promise) {
-          createPromise(); // Ensure the promise is instantiated
-        }
-        return _promise;
-      },
-      enumerable: true
-    },
-    resolve: {
-      value: resolve,
-      enumerable: true
-    },
-    reject: {
-      value: reject,
-      enumerable: true
-    },
-    reset: {
-      value: reset,
-      enumerable: true
-    },
-    then: {
-      value: function (onFulfilled: (value: T) => void, onRejected?: (reason: any) => void) {
-        return fn.promise.then(onFulfilled, onRejected);
-      },
-      enumerable: true
-    }
-  });
-
-  return fn;
+  return innerFunction;
 }
 
 promisified.all = function (promises: Array<ReturnType<typeof promisified<any>>>): Promise<any[]> {
