5437bad1ab60fdc845db22f5fc5f89b410815dee
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-08-02 14:35:55 +0300

    feat: add index to scan operator

--- projects/streamix/src/lib/operators/scan.ts
+++ projects/streamix/src/lib/operators/scan.ts
@@ -3,11 +3,12 @@ import { Emission } from '../abstractions/emission';
 import { Operator } from '../abstractions/operator';
 
 export class ScanOperator extends Operator {
-  private readonly accumulator: (acc: any, value: any) => any;
+  private readonly accumulator: (acc: any, value: any, index?: number) => any;
   private readonly seed: any;
   private accumulatedValue: any;
+  private index = 0;
 
-  constructor(accumulator: (acc: any, value: any) => any, seed: any) {
+  constructor(accumulator: (acc: any, value: any, index?: number) => any, seed: any) {
     super();
     this.accumulator = accumulator;
     this.seed = seed;
@@ -15,11 +16,11 @@ export class ScanOperator extends Operator {
   }
 
   async handle(emission: Emission, stream: Stream): Promise<Emission> {
-    this.accumulatedValue = this.accumulator(this.accumulatedValue, emission.value!);
+    this.accumulatedValue = this.accumulator(this.accumulatedValue, emission.value!, this.index++);
     emission.value = this.accumulatedValue;
     return emission;
   }
 }
 
-export const scan = (accumulator: (acc: any, value: any) => any, seed: any) => new ScanOperator(accumulator, seed);
+export const scan = (accumulator: (acc: any, value: any, index?: number) => any, seed: any) => new ScanOperator(accumulator, seed);
 
