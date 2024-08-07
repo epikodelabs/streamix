2b4a71cf209b15752437d9f73b59c61385d51d86
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-08-07 14:53:52 +0300

    refactor: call promise.race once

--- projects/streamix/src/lib/operators/iif.ts
+++ projects/streamix/src/lib/operators/iif.ts
@@ -8,6 +8,7 @@ export class IifOperator extends Operator {
 
   private input?: Subscribable;
   private output?: Stream;
+  private innerStream?: Subscribable;
   private innerSubscription?: { unsubscribe: () => void };
 
   constructor(
@@ -30,10 +31,6 @@ export class IifOperator extends Operator {
       this.innerSubscription.unsubscribe();
     }
 
-    if (this.outerStream) {
-      await this.outerStream.complete();
-    }
-
     if (this.input) {
       await this.input.complete();
     }
@@ -44,25 +41,27 @@ export class IifOperator extends Operator {
   }
 
   async handle(emission: Emission, stream: Subscribable): Promise<Emission> {
-    this.input = this.input || stream;
-    this.output = this.output || this.outerStream;
+    if (!this.input) {
+      this.input = stream;
+      this.output = this.outerStream;
 
-    const innerStream = this.condition(emission) ? this.trueStream : this.falseStream;
+      this.innerStream = this.condition(emission) ? this.trueStream : this.falseStream;
 
-    this.innerSubscription = innerStream.subscribe(async (value) => {
-      await this.output!.emit({value}, this.output?.head!);
-    });
+      this.innerSubscription = this.innerStream.subscribe(async (value) => {
+        await this.output!.emit({value}, this.output?.head!);
+      });
 
-    Promise.race([innerStream.awaitCompletion(), innerStream.awaitTermination()]).then((error) => {
-      this.innerSubscription?.unsubscribe();
-      this.output?.complete();
-      this.input?.complete();
-    });
+      Promise.race([this.innerStream.awaitCompletion(), this.innerStream.awaitTermination()]).then((error) => {
+        this.innerSubscription?.unsubscribe();
+        this.output?.complete();
+        this.input?.complete();
+      });
+    }
 
     emission.isPhantom = true;
 
     return new Promise<Emission>((resolve) => {
-      innerStream.isStopped.then(() => resolve(emission));
+      this.innerStream?.isStopped.then(() => resolve(emission));
     });
   }
 }
