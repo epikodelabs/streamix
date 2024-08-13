2afa25f895e19e3598ac49ae1565fb9451048f36
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-08-13 18:42:30 +0300

    refactor: make chunk extend stream

--- projects/streamix/src/lib/abstractions/chunk.ts
+++ projects/streamix/src/lib/abstractions/chunk.ts
@@ -1,91 +1,40 @@
 import { Hook, Stream } from '../abstractions';
-import { hook, HookType, promisified, PromisifiedType } from '../utils';
+import { hook } from '../utils';
 import { Emission } from './emission';
 import { Operator } from './operator';
 import { Subscribable } from './subscribable';
 import { Subscription } from './subscription';
 
-export class Chunk<T = any> implements Subscribable<T> {
+export class Chunk<T = any> extends Stream<T> implements Subscribable<T> {
   operators: Operator[] = [];
   head: Operator | undefined;
   tail: Operator | undefined;
 
   constructor(public stream: Stream<T>) {
+    super();
+    Object.assign(this, stream);
+    this.subscribers = hook();
   }
 
-  get isAutoComplete(): PromisifiedType<boolean> {
-    return this.stream.isAutoComplete;
-  }
-  get isCancelled(): PromisifiedType<boolean> {
-    return this.stream.isCancelled;
-  }
-  get isStopRequested(): PromisifiedType<boolean> {
-    return this.stream.isStopRequested;
-  }
-  get isFailed(): PromisifiedType<any> {
-    return this.stream.isFailed;
-  }
-  get isStopped(): PromisifiedType<boolean> {
-    return this.stream.isStopped;
-  }
-  get isUnsubscribed(): PromisifiedType<boolean> {
-    return this.stream.isUnsubscribed;
-  }
-  get isRunning(): PromisifiedType<boolean> {
-    return this.stream.isRunning;
-  }
-  get subscribers(): HookType {
-    return this.stream.subscribers;
-  }
-
-  run(): Promise<void> {
+  override run(): Promise<void> {
     return this.stream.run();
   }
 
-  shouldTerminate() {
-    return this.isCancelled() || this.isFailed();
-  }
-
-  awaitTermination() {
-    return promisified.race([this.isCancelled, this.isFailed]);
-  }
-
-  terminate(): Promise<void> {
-    this.isCancelled.resolve(true);
-    return this.isStopped.then(() => Promise.resolve());
-  }
-
-  shouldComplete() {
-    return this.isAutoComplete() || this.isUnsubscribed() || this.isStopRequested();
-  }
-
-  awaitCompletion() {
-    return promisified.race([this.isAutoComplete, this.isUnsubscribed, this.isStopRequested]);
-  }
-
-  complete(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      const timer = setTimeout(() => {
-        this.isStopRequested.resolve(true);
-        this.isStopped.then(() => resolve());
-      }, 0);
-    });
-  }
 
   // Protected method to handle the subscription chain
-  subscribe(callback: ((value: T) => any) | void): Subscription {
-    if(!this.stream.onEmission.contains(this, this.emit)) {
-      this.stream.onEmission.chain(this, this.emit);
+  override subscribe(callback: ((value: T) => any) | void): Subscription {
+    if(!this.onEmission.contains(this, this.emit)) {
+      this.onEmission.chain(this, this.emit);
     }
 
-    const subscription = this.stream.subscribe(callback, this);
+    const subscription = super.subscribe(callback, this);
 
     return {
       unsubscribe: () => subscription.unsubscribe()
     };
   }
 
-  pipe(...operators: Operator[]): Subscribable<T> {
+  override pipe(...operators: Operator[]): Subscribable<T> {
     operators = [...this.operators, ...operators];
     this.operators = []; this.head = undefined; this.tail = undefined;
     operators.forEach((operator, index) => {
@@ -116,7 +65,7 @@ export class Chunk<T = any> implements Subscribable<T> {
     return this;
   }
 
-  async emit({ emission, source }: { emission: Emission; source: any }): Promise<void> {
+  override async emit({ emission, source }: { emission: Emission; source: any }): Promise<void> {
     try {
       let next = (source instanceof Stream) ? this.head : undefined;
       next = (source instanceof Operator) ? source.next : next;
@@ -139,9 +88,8 @@ export class Chunk<T = any> implements Subscribable<T> {
       emission.isFailed = true;
       emission.error = error;
 
-      const stream = this.stream;
-      if (stream.onError.length > 0) {
-        await stream.onError.process({ error });
+      if (this.onError.length > 0) {
+        await this.onError.process({ error });
       } else {
         this.isFailed.resolve(error);
       }
