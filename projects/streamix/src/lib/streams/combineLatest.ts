41f6de9f2effaa36df6a502da16c87c92c7e165c
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-10-08 14:20:17 +0300

    refactor: rewrite combineLatest stream

--- projects/streamix/src/lib/streams/combineLatest.ts
+++ projects/streamix/src/lib/streams/combineLatest.ts
@@ -1,71 +1,61 @@
 import { Stream, Subscribable, Subscription } from '../abstractions';
 
 export class CombineLatestStream<T = any> extends Stream<T[]> {
-  private readonly sources: Subscribable[];
-  private subscriptions: Subscription[] = [];
-  private values: { hasValue: boolean, value: any }[];
+  private readonly sources: Stream[];
+  private values: { hasValue: boolean; value: any }[];
   private remaining: number;
+  private handleEmissionFns: Array<(value: any) => void> = [];
 
-  constructor(sources: Subscribable[]) {
+  constructor(sources: Stream[]) {
     super();
     this.sources = sources;
     this.values = sources.map(() => ({ hasValue: false, value: undefined }));
     this.remaining = sources.length;
-  }
 
-  override async run(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      this.sources.forEach((source, index) => {
-        const subscription = source.subscribe((value: any) => {
-          if (this.sources.every(source => source.shouldComplete() || source.shouldTerminate())) {
-            resolve();
-            return;
-          }
-
-          if (!this.values[index].hasValue) {
-            this.remaining--;
-          }
-
-          this.values[index].hasValue = true;
-          this.values[index].value = value;
-
-          if (this.remaining === 0) {
-            this.onEmission.process({ emission: { value: this.values.map(({ value }) => value) }, source: this });
-          }
-        });
-
-        this.subscriptions.push(subscription);
-      });
-
-      Promise.race([this.awaitCompletion(), this.awaitTermination()]).then(() => {
-        this.subscriptions.forEach(subscription => subscription.unsubscribe());
-        resolve();
-      });
+    // Store the complete callback as instance properties for each source
+    this.sources.forEach((source, index) => {
+      this.handleEmissionFns[index] = ({ emission, source }) => this.handleEmission(index, emission.value);
+      source.onEmission.chain(this, this.handleEmissionFns[index]);
     });
   }
 
-  override subscribe(callback: (value: T[]) => any): Subscription {
-    const subscription = super.subscribe(callback);
-    // Return a subscription object with an unsubscribe method
-    return {
-      unsubscribe: () => {
-        subscription.unsubscribe();
+  override async run(): Promise<void> {
+    this.sources.forEach((source, index) => {
+      source.start(source);
+    });
 
-        if (this.subscribers.length !== 0) {
-          // Ensure all source streams are stopped
-          this.sources.forEach(source => {
-            source.isStopRequested.resolve(true);
-            source.isUnsubscribed.resolve(true);
-          });
+    return Promise.race([this.awaitCompletion(), this.awaitTermination()]).then(() => {
+      this.clearAllChains();
+    });
+  }
 
-          // Unsubscribe from all sources
-          this.subscriptions.forEach(subscription => subscription.unsubscribe());
-        }
-      }
-    };
+  private async handleEmission(index: number, value: any): Promise<void> {
+    if (this.sources.every((source) => source.shouldComplete() || source.shouldTerminate())) {
+      return;
+    }
+
+    if (!this.values[index].hasValue) {
+      this.remaining--;
+    }
+
+    this.values[index].hasValue = true;
+    this.values[index].value = value;
+
+    if (this.remaining === 0) {
+      await this.onEmission.process({
+        emission: { value: this.values.map(({ value }) => value) },
+        source: this,
+      });
+    }
+  }
+
+  private clearAllChains(): void {
+    this.sources.forEach((source, index) => {
+      source.terminate();
+    });
   }
 }
 
-export function combineLatest<T = any>(sources: Subscribable[]) {
+export function combineLatest<T = any>(sources: Stream[]) {
   return new CombineLatestStream<T>(sources);
 }
