9ed20bb1ecd6f94407ebd190e4fc18b6a41a3d6f
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-10-09 11:24:28 +0300

    refactor: reimplement timer

--- projects/streamix/src/lib/streams/timer.ts
+++ projects/streamix/src/lib/streams/timer.ts
@@ -1,70 +1,100 @@
 import { Stream } from '../abstractions/stream';
-import { Subscription } from '../abstractions/subscription';
 
-export class TimerStream<T = any> extends Stream<T> {
+export class TimerStream extends Stream<number> {
   private delayMs: number;
-  private intervalId: any;
   private intervalMs: number;
-  private value: number;
-  private resolvePromise: ((value: void | PromiseLike<void>) => void) | null = null;
+  private value: number = 0;
+  private timeoutId: any | undefined;
+  private intervalId: any | undefined;
 
   constructor(delayMs: number = 0, intervalMs?: number) {
     super();
     this.delayMs = delayMs;
-    this.intervalMs = intervalMs || delayMs;
-    this.intervalId = null;
-    this.value = 0;
+    this.intervalMs = intervalMs ?? delayMs;
   }
 
   override async run(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      this.resolvePromise = resolve;
+    try {
+      if (await this.initialDelay()) {
+        return; // Stream was completed or terminated during the delay
+      }
 
-      setTimeout(() => {
-        this.onEmission.process({ emission: { value: this.value }, source: this }).then(() => {
-          this.intervalId = setInterval(() => {
-            this.value++;
-            this.onEmission.process({ emission: { value: this.value }, source: this });
-          }, this.intervalMs);
-        });
+      await this.emitValue();
+
+      if (this.intervalMs > 0) {
+        await this.startInterval();
+      } else {
+        this.isAutoComplete.resolve(true);
+      }
+    } catch (error) {
+      await this.handleError(error);
+    } finally {
+      this.cleanup();
+    }
+  }
+
+  private async initialDelay(): Promise<boolean> {
+    if (this.delayMs === 0) {
+      return this.shouldComplete() || this.shouldTerminate();
+    }
+
+    return new Promise<boolean>((resolve) => {
+      this.timeoutId = setTimeout(() => {
+        this.timeoutId = undefined;
+        resolve(this.shouldComplete() || this.shouldTerminate());
       }, this.delayMs);
-
-      // Listen for the stop request
-      this.isStopRequested.then(() => {
-        if (this.intervalId) {
-          clearInterval(this.intervalId);
-          this.intervalId = null;
-        }
-        if (this.resolvePromise) {
-          this.resolvePromise();
-          this.resolvePromise = null;
-        }
-      });
     });
   }
 
-  override subscribe(callback: (value: T) => any): Subscription {
-    let subscription = super.subscribe(callback);
+  private async emitValue(): Promise<void> {
+    if (this.shouldComplete() || this.shouldTerminate()) {
+      return;
+    }
+    await this.onEmission.process({ emission: { value: this.value }, source: this });
+    this.value++;
+  }
 
-    // Return a subscription object with an unsubscribe method
-    return {
-      unsubscribe: () => {
-        subscription.unsubscribe();
-        if (this.subscribers.length === 0) {
-          if (this.intervalId) {
-            clearInterval(this.intervalId);
-            this.intervalId = null;
+  private startInterval(): Promise<void> {
+    return new Promise<void>((resolve) => {
+      this.intervalId = setInterval(async () => {
+        try {
+          if (this.shouldComplete() || this.shouldTerminate()) {
+            this.cleanup();
+            resolve();
+            return;
           }
-          if (this.resolvePromise) {
-            this.resolvePromise();
-            this.resolvePromise = null;
-          }
+          await this.emitValue();
+        } catch (error) {
+          this.handleError(error);
+          this.cleanup();
+          resolve();
         }
-      }
-    };
+      }, this.intervalMs);
+    });
+  }
+
+  private cleanup(): void {
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
+      this.timeoutId = undefined;
+    }
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = undefined;
+    }
+  }
+
+  override async complete(): Promise<void> {
+    this.cleanup();
+    return super.complete();
+  }
+
+  override async terminate(): Promise<void> {
+    this.cleanup();
+    return super.terminate();
   }
 }
 
-export function timer<T = any>(delayMs: number = 0, interval?: number) {
-  return new TimerStream<T>(delayMs, interval);
+export function timer(delayMs: number = 0, intervalMs?: number): TimerStream {
+  return new TimerStream(delayMs, intervalMs);
 }
