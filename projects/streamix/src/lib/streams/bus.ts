040296db5b5f1efdae4b249fb857842fde7f9071
Oleksii Shepel <oleksii.shepel@outlook.com>
2024-11-13 16:19:19 +0200

    refactor: update bus implementation

--- projects/streamix/src/lib/streams/bus.ts
+++ projects/streamix/src/lib/streams/bus.ts
@@ -2,7 +2,7 @@ import { createStream, Emission, Stream } from '../abstractions';
 import { promisified, PromisifiedType } from '../utils';
 
 export const eventBus = createBus() as Bus;
-eventBus.subscribe();
+eventBus.run();
 
 // Define a functional lock for synchronizing access
 export function createLock() {
@@ -21,12 +21,13 @@ export function createLock() {
 
 export type BusEvent = {
   target: any,
-  payload: any,
   type: 'emission' | 'start' | 'stop' | 'complete' | 'error';
+  payload?: any,
   timeStamp?: Date
 };
 
-export type Bus<T = any> = Stream<T> & {
+export type Bus = {
+  run(): Promise<void>;
   enqueue(event: BusEvent): Promise<void>;
 };
 
@@ -42,87 +43,76 @@ export function createBus(): Bus {
 
   const lock = createLock(); // Functional lock for controlling access
 
-  const bus = createStream(async function (this: any): Promise<void> {
-    spaceAvailable.resolve(); // Initially, space is available
+  const bus = {
+    run: async function (this: any): Promise<void> {
+      spaceAvailable.resolve(); // Initially, space is available
 
-    while (true) {
-      // Wait for the next emission or completion signal
-      await Promise.race([emissionAvailable.promise(), this.awaitCompletion()]);
+      while (true) {
+        // Wait for the next emission or completion signal
+        await emissionAvailable.promise();
 
-      if (this.shouldComplete() && itemsCount === 0) {
-        break; // Exit the loop if there are no buffered values and the stream should complete
+        // Process each buffered value sequentially
+        while (itemsCount > 0) {
+          const promisifiedValue = buffer[head];
+          if (promisifiedValue) {
+            const event = promisifiedValue() as BusEvent;
+
+            switch(event.type) {
+              case 'start': await event.target.onStart.parallel(event.payload); break;
+              case 'stop': await event.target.onStop.parallel(event.payload); break;
+              case 'emission': await event.target.onEmission.parallel(event.payload); break;
+              case 'complete': await event.target.onComplete.parallel(event.payload); break;
+              case 'error': await event.target.onError.parallel(event.payload); break;
+            }
+
+            promisifiedValue.resolve(event);
+
+            // Move the head forward in the cyclic buffer and reduce the count
+            head = (head + 1) % bufferSize;
+            itemsCount--;
+
+            // Resolve the spaceAvailable promise if there's space now
+            if (itemsCount < bufferSize) {
+              spaceAvailable.resolve();
+            }
+          }
+
+          // Reset `emissionAvailable` after processing all buffered values
+          emissionAvailable.reset();
+        }
       }
+    },
 
-      // Process each buffered value sequentially
-      while (itemsCount > 0) {
-        const promisifiedValue = buffer[head];
-        if (promisifiedValue) {
-          const event = promisifiedValue() as BusEvent;
+    enqueue: async function (this: Bus, event: BusEvent): Promise<void> {
+      // Acquire the lock before proceeding
+      const releaseLock = await lock.acquire();
 
-          switch(event.type) {
-            case 'start': event.target.onStart.parallel(event.payload); break;
-            case 'stop': event.target.onStop.parallel(event.payload); break;
-            case 'emission': event.target.onEmission.parallel(event.payload); break;
-            case 'complete': event.target.onComplete.parallel(event.payload); break;
-            case 'error': event.target.onError.parallel(event.payload); break;
-          }
-
-          promisifiedValue.resolve(event);
-
-          // Move the head forward in the cyclic buffer and reduce the count
-          head = (head + 1) % bufferSize;
-          itemsCount--;
-
-          // Resolve the spaceAvailable promise if there's space now
-          if (itemsCount < bufferSize) {
-            spaceAvailable.resolve();
-          }
+      try {
+        // Wait until there is space in the buffer
+        if (itemsCount === bufferSize) {
+          await spaceAvailable.promise();
+          spaceAvailable.reset();
         }
 
-        // Reset `emissionAvailable` after processing all buffered values
-        emissionAvailable.reset();
+        event.timeStamp = new Date();
+        const promisifiedValue = promisified<any>(event);
+
+        // Place the new value at the tail and advance the tail position
+        buffer[tail] = promisifiedValue;
+        tail = (tail + 1) % bufferSize;
+        itemsCount++;
+
+        // Resolve emissionAvailable if the buffer was empty
+        emissionAvailable.resolve();
+
+        return promisifiedValue.then(() => Promise.resolve());
+      } finally {
+        releaseLock(); // Release the lock after finishing
       }
-    }
+    },
 
-    if (!this.shouldComplete()) {
-      this.isAutoComplete = true;
-    }
-  }) as Bus;
-
-  bus.enqueue = async function (this: Stream, event: BusEvent): Promise<void> {
-    // If the stream is stopped, further emissions are not allowed
-    if (this.isStopRequested || this.isStopped) {
-      console.warn('Cannot push value to a stopped bus.');
-      return Promise.resolve();
-    }
-
-    // Acquire the lock before proceeding
-    const releaseLock = await lock.acquire();
-
-    try {
-      // Wait until there is space in the buffer
-      if (itemsCount === bufferSize) {
-        await spaceAvailable.promise();
-        spaceAvailable.reset();
-      }
-
-      event.timeStamp = new Date();
-      const promisifiedValue = promisified<any>(event);
-
-      // Place the new value at the tail and advance the tail position
-      buffer[tail] = promisifiedValue;
-      tail = (tail + 1) % bufferSize;
-      itemsCount++;
-
-      // Resolve emissionAvailable if the buffer was empty
-      emissionAvailable.resolve();
-
-      return promisifiedValue.then(() => Promise.resolve());
-    } finally {
-      releaseLock(); // Release the lock after finishing
-    }
+    name: "bus"
   };
 
-  bus.name = "bus";
   return bus;
 }
