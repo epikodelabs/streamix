8a1eb295b60172000ee766993feeda7299de85d4
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-11-23 10:59:21 +0200

    refactor: update startWith.ts

--- projects/streamix/src/lib/hooks/startWith.ts
+++ projects/streamix/src/lib/hooks/startWith.ts
@@ -11,7 +11,6 @@ export const startWith = (value: any): Operator => {
 
   const callback = (instance: Operator, params?: any): void => {
     // Emit the provided initial value when the stream starts
-    boundStream.emissionCounter++;
     eventBus.enqueue({ target: boundStream, payload: { emission: createEmission({ value }), source: instance }, type: 'emission' });
   };
 
