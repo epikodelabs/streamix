4bc4ed63b497606a9c263c3f56da8066b013af51
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-11-26 20:09:49 +0200

    refactor: update stream.ts

--- projects/streamix/src/lib/abstractions/stream.ts
+++ projects/streamix/src/lib/abstractions/stream.ts
@@ -103,8 +103,6 @@ export function createStream<T = any>(runFn: (this: Stream<T>, params?: any) => 
 
   const emit = async function({ emission, source }: { emission: Emission; source: any }): Promise<void> {
     try {
-      emissionCounter++;
-
       let next = isStream(source) ? source.head : undefined;
       next = isOperator(source) ? source.next : next;
 
@@ -117,6 +115,7 @@ export function createStream<T = any>(runFn: (this: Stream<T>, params?: any) => 
       if (emission.failed) throw emission.error;
 
       if (!emission.phantom && !emission.pending) {
+        emissionCounter++;
         await subscribers.parallel({ emission, source });
       }
 
