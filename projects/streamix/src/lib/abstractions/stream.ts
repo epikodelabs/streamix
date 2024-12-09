63a0f4f73cab19004947fb4c3afbe2029b9ba298
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-12-09 06:18:00 +0200

    refactor: update stream.ts

--- projects/streamix/src/lib/abstractions/stream.ts
+++ projects/streamix/src/lib/abstractions/stream.ts
@@ -74,6 +74,8 @@ export function createStream<T = any>(runFn: (this: Stream<T>, params?: any) => 
       stream[flags].isUnsubscribed = true;
       if (!stopped) {
         await finalize.waitForCompletion();
+        finalize.clear();
+        subscribers.clear();
       }
     }
   };
