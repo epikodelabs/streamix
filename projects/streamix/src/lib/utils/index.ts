57882b36ba7685bde4acb042e2ca8ceeacd8480f
actioncrew <sepeloleksij4@gmail.com>
2025-01-06 10:32:54 +0200

    refactor: delete hook

--- projects/streamix/src/lib/utils/index.ts
+++ projects/streamix/src/lib/utils/index.ts
@@ -2,7 +2,6 @@ export * from './awaitable';
 export * from './catch';
 export * from './counter';
 export * from './emitter';
-export * from './hook';
 export * from './lock';
 export * from './semaphore';
 export * from './value';
