5030a3cc7dfb3e532094310dddf4b09069841afd
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-02-20 05:31:14 +0200

    refactor: name stream properly

--- projects/streamix/src/lib/streams/webSocket.ts
+++ projects/streamix/src/lib/streams/webSocket.ts
@@ -11,7 +11,7 @@ export interface WebSocketStream<T = any> extends Stream<T> {
  * @param url - The WebSocket URL to connect to.
  * @returns A WebSocketStream with message emission and sending capabilities.
  */
-export function webSocketStream<T = any>(url: string): WebSocketStream<T> {
+export function webSocket<T = any>(url: string): WebSocketStream<T> {
   const output = createSubject<T>(); // Use subject to emit messages to subscribers
   let socket: WebSocket | null = new WebSocket(url);
   const queue: T[] = [];
@@ -51,5 +51,5 @@ export function webSocketStream<T = any>(url: string): WebSocketStream<T> {
   socket.addEventListener("message", handleMessage);
   socket.addEventListener("close", handleClose);
 
-  return Object.assign(output, { send: sendMessage }) as WebSocketStream<T>;
+  return Object.assign(output, { name: "webSocket", send: sendMessage }) as WebSocketStream<T>;
 }
