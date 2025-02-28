e131dd230ca498485f4d1c3fdd599d6061b8318a
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-03-01 01:32:00 +0200

    Update index.ts

--- projects/angular/src/lib/index.ts
+++ projects/angular/src/lib/index.ts
@@ -1,9 +1,9 @@
 /*
  * Public API Surface of angular
  */
-
+export * from './asyncPipe';
+export * from './eventEmitter';
 export * from './stream.directive';
 export * from './stream.module';
 export * from './stream.pipe';
-export * from './eventEmitter';
 
