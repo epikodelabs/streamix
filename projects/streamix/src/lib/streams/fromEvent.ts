582fb67bfb5dfb3fc8754a5d2ad746521cf42e14
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-03-07 13:50:48 +0200

    fix: generic defaults to Event

--- projects/streamix/src/lib/streams/fromEvent.ts
+++ projects/streamix/src/lib/streams/fromEvent.ts
@@ -1,24 +1,16 @@
 import { createSubscription, Receiver, Stream } from "../abstractions";
 import { createSubject } from '../streams';
 
-/**
- * Creates a stream from an event target, where events trigger emissions in the stream.
- *
- * @param target - The EventTarget (e.g., DOM element or window).
- * @param event - The event type (e.g., 'click', 'resize').
- * @returns A stream that emits the event when it occurs.
- */
-export function fromEvent<T>(target: EventTarget, event: string): Stream<T> {
-  const subject = createSubject<T>(); // Create a subject to emit event values.
+export function fromEvent(target: EventTarget, event: string): Stream<Event> {
+  const subject = createSubject<Event>(); // Create a subject to emit event values.
 
   const originalSubscribe = subject.subscribe; // Capture original subscribe method.
-
-  const subscribe = (callback?: ((value: T) => void) | Receiver<T>) => {
+  const subscribe = (callback?: ((value: Event) => void) | Receiver<Event>) => {
     const subscription = originalSubscribe.call(subject, callback);
 
     const listener = (ev: Event) => {
       if (!subject.completed()) {
-        subject.next(ev as T); // Emit the event directly into the subject's stream
+        subject.next(ev); // Emit the event directly into the subject's stream
       }
     };
 
