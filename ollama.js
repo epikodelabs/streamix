389daa429848b534247cff9d437f8b00771ca476
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-03-12 12:18:21 +0200

    corrected path to CHANGELOG.md in ollama script

--- ollama.js
+++ ollama.js
@@ -257,7 +257,7 @@ async function main() {
   fs.rmSync('summary_changelog.json');
 }
 
-function generateChangelogFromSummary(summaryFilePath = 'summary_changelog.json', outputFilePath = './projects/streamix/CHANGELOG.md') {
+function generateChangelogFromSummary(summaryFilePath = 'summary_changelog.json', outputFilePath = './projects/libraries/streamix/CHANGELOG.md') {
   try {
     const summaryData = fs.readFileSync(summaryFilePath, 'utf-8');
     const summary = JSON.parse(summaryData);
