791e2b04414b9e78ed5c27e05890d27052ec80b7
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-03-19 08:49:37 +0200

    extended receiver with unsubscribe method

--- projects/libraries/streamix/src/lib/abstractions/receiver.ts
+++ projects/libraries/streamix/src/lib/abstractions/receiver.ts
@@ -1,8 +1,11 @@
 export type Receiver<T = any> = {
+  value?: () => T | undefined;
   next?: (value: T) => void;
   error?: (err: Error) => void;
   complete?: () => void;
   unsubscribed?: boolean;
+  completed?: boolean;
+  unsubscribe?: () => void;
 };
 
 export function createReceiver<T = any>(callbackOrReceiver?: ((value: T) => void) | Receiver<T>): Required<Receiver<T>> {
@@ -10,10 +13,21 @@ export function createReceiver<T = any>(callbackOrReceiver?: ((value: T) => void
     { next: callbackOrReceiver } :
     callbackOrReceiver || {}) as Required<Receiver<T>>;
 
-  receiver.next = receiver.next ?? (() => {});
-  receiver.error = receiver.error ?? ((err) => console.error('Unhandled error:', err));
-  receiver.complete = receiver.complete ?? (() => {});
+  let latestValue: T | undefined;
+
   receiver.unsubscribed = false;
+  receiver.completed = false;
+
+  receiver.value = () => latestValue;
+
+  const originalNext = receiver.next;
+  const originalComplete = receiver.complete;
+  const originalUnsubscribe = receiver.unsubscribe;
+
+  receiver.next = function (this: Receiver, value: T) { latestValue = value; originalNext?.call(this, value); }
+  receiver.error = receiver.error ?? ((err) => console.error('Unhandled error:', err));
+  receiver.complete = function (this: Receiver) { this.completed = true; originalComplete?.call(this); };
+  receiver.unsubscribe = function (this: Receiver) { this.unsubscribed = true; originalUnsubscribe?.call(this); };
 
   return receiver;
 }
