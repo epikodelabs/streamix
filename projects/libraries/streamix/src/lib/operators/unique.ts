150414bacbf73f9b93ac95eed867587a904e1a40
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-04-01 17:24:51 +0300

    added unique operator

---
+++ projects/libraries/streamix/src/lib/operators/unique.ts
@@ -0,0 +1,31 @@
+import { createMapper, Stream, StreamMapper } from "../abstractions";
+import { eachValueFrom } from "../converters";
+import { createSubject } from "../streams";
+
+export function unique<T = any, K = any>(keySelector?: (value: T) => K): StreamMapper {
+  const operator = (input: Stream<T>): Stream<T> => {
+    const output = createSubject<T>();
+    const seenKeys = new Set<K | T>();
+
+    (async() {
+      try {
+        for await (const value of eachValueFrom(input)) {
+          const currentKey = keySelector ? keySelector(value) : value;
+
+          if (!seenKeys.has(currentKey)) {
+            output.next(value);
+            seenKeys.add(currentKey);
+          }
+        }
+        output.complete();
+      } catch (err) {
+        output.error(err);
+      }
+    })();
+
+    return output;
+  };
+
+  return createMapper('unique', operator);
+}
+          
