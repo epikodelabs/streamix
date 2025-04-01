e39cd8ac1c88e2a33e20c5f1778186f8b3ed08ef
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-04-01 19:54:52 +0300

    added select operator

---
+++ projects/libraries/streamix/src/lib/operators/select.ts
@@ -0,0 +1,39 @@
+import { createMapper, Stream, StreamMapper } from "../abstractions";
+import { eachValueFrom } from "../converters";
+import { createSubject } from "../streams";
+
+export function select<T = any>(indexIterator: Iterator<number>): StreamMapper {
+  const operator = (input: Stream<T>): Stream<T> => {
+    const output = createSubject<T>();
+    let currentIndex = 0;
+    let nextIndex = indexIterator.next().value;
+
+    (async () => {
+      try {
+        for await (const value of eachValueFrom(input)) {
+          if (nextIndex === undefined) {
+            output.complete();
+            break;
+          }
+
+          if (currentIndex === nextIndex) {
+            output.next(value);
+            nextIndex = indexIterator.next().value;
+          }
+
+          currentIndex++;
+        }q
+      } catch (err) {
+        output.error(err);
+      } finally {
+        if (!output.completed()) {
+          output.complete();
+        }
+      }
+    })();
+
+    return output;
+  };
+
+  return createMapper('select', operator);
+}
