22d12557601fa36ab71df4b19222937aac1556fb
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-04-02 10:29:17 +0300

    fix(elementNth): align indexed element selection behavior

--- projects/libraries/streamix/src/lib/operators/elementNth.ts
+++ projects/libraries/streamix/src/lib/operators/elementNth.ts
@@ -1,6 +1,5 @@
-import { createMapper, Stream, StreamMapper } from "../abstractions";
-import { eachValueFrom } from "../converters";
-import { createSubject } from "../streams";
+import { StreamMapper } from "../abstractions";
+import { select } from "../operators";
 
 export function elementNth<T = any>(indexPattern: (index: number) => boolean): StreamMapper {
   const indexIterator = (function* () {
