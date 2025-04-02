46ffcdd40f3adc5942515196d11ea1fb1f504e80
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-04-02 13:21:26 +0300

    implemented buffer operator

---
+++ projects/libraries/streamix/src/lib/operators/buffer.ts
@@ -0,0 +1,43 @@
+import { createMapper, Stream, StreamMapper, Subscription } from "../abstractions";
+import { createSubject, interval } from "../streams";
+
+export function bufferTime<T = any>(period: number): StreamMapper {
+  return createMapper('bufferTime', (input: Stream<T>): Stream<T[]> => {
+    const output = createSubject<T[]>();
+    let buffer: T[] = [];
+    let intervalSubscription: Subscription | null = null;
+    let inputSubscription: Subscription | null = null;
+
+    intervalSubscription = interval(period).subscribe({
+      next: () => {
+        if (buffer.length > 0) {
+          output.next([...buffer]);
+          buffer = [];
+        }
+      },
+      error: (err) => output.error(err),
+      complete: () => {
+        if (buffer.length > 0) {
+          output.next([...buffer]);
+        }
+        output.complete();
+      },
+    });
+
+    inputSubscription = input.subscribe({
+      next: (value) => {
+        buffer.push(value);
+      },
+      error: (err) => output.error(err),
+      complete: () => {
+        if (buffer.length > 0) {
+          output.next([...buffer]);
+        }
+        intervalSubscription?.unsubscribe();
+        output.complete();
+      },
+    });
+
+    return output;
+  });
+                                     }
