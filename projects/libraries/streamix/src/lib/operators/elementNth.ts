7cc4efc263b6743c5c06eb2dcbae2a8d2a1dcb86
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-04-02 10:08:25 +0300

    elementNth expects predicate

--- projects/libraries/streamix/src/lib/operators/elementNth.ts
+++ projects/libraries/streamix/src/lib/operators/elementNth.ts
@@ -2,15 +2,21 @@ import { createMapper, Stream, StreamMapper } from "../abstractions";
 import { eachValueFrom } from "../converters";
 import { createSubject } from "../streams";
 
-export function elementNth<T = any>(indexPattern: (index: number) => number | undefined): StreamMapper {
+import { createMapper, Stream, StreamMapper } from "../abstractions";
+import { eachValueFrom } from "../converters";
+import { createSubject } from "../streams";
+
+export function elementNth<T = any>(indexPattern: (index: number) => boolean): StreamMapper {
   const indexIterator = (function* () {
     let index = 0;
     while (true) {
-      const nextIndex = indexPattern(index++);
-      if (nextIndex === undefined) break;
-      yield nextIndex;
+      if (indexPattern(index)) {
+        yield index;
+      }
+      index++;
     }
   })();
 
   return select(indexIterator);
 }
+
