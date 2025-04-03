c9ff2e2ed41ccee9cba05ed33b0be49925c78d2c
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-04-03 11:30:55 +0300

    added options to expand

--- projects/libraries/streamix/src/lib/operators/expand.ts
+++ projects/libraries/streamix/src/lib/operators/expand.ts
@@ -2,13 +2,15 @@ import { createMapper, Stream, StreamMapper } from "../abstractions";
 import { recurse } from "../operators";
 
 export function expand<T = any>(
-  project: (value: T) => Stream<T>
+  project: (value: T) => Stream<T>,
+  options: { traversal?: 'depth' | 'breadth', maxDepth?: number; } = {}
 ): StreamMapper {
   return createMapper('expand', (input: Stream<T>) => {
     return input.pipe(
       recurse(
         () => true, // Always continue recursion
-        value => project(value)
+        value => project(value),
+        options
       )
     );
   });
