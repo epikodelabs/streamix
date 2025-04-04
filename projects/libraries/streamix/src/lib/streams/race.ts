7594db1e1f728f56b747eff3c4901da23f2ec55c
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-04-04 09:06:09 +0300

    added tes cases for race stream

--- projects/libraries/streamix/src/lib/streams/race.ts
+++ projects/libraries/streamix/src/lib/streams/race.ts
@@ -1,47 +1,59 @@
 import { Stream } from "../abstractions";
 import { createSubject } from "../streams";
 
-/**
- * Emits values from the first stream that emits, ignoring all other streams
- * @param streams Array of streams to race
- * @returns Stream that emits values from the winning stream
- */
-export function race<T>(streams: Stream<T>[]): Stream<T> {
+export function race<T>(...streams: Stream<T>[]): Stream<T> {
   const subject = createSubject<T>();
-  let winnerFound = false;
+  let winnerIndex = -1;
   const subscriptions: { unsubscribe: () => void }[] = [];
 
-  streams.forEach(stream => {
+  // Helper to unsubscribe all except the winner
+  const unsubscribeOthers = (winnerSub: { unsubscribe: () => void }) => {
+    for (const s of subscriptions) {
+      if (s !== winnerSub) s.unsubscribe();
+    }
+  };
+
+  streams.forEach((stream, index) => {
     const sub = stream.subscribe({
       next: (value) => {
-        if (!winnerFound) {
-          winnerFound = true;
-          // Unsubscribe all other streams
-          subscriptions.forEach(s => {
-            if (s !== sub) s.unsubscribe();
-          });
-        }
-        subject.next(value);
+        // Delay winner selection to allow all subs to register
+          if (winnerIndex === -1) {
+            winnerIndex = index;
+            queueMicrotask(() => {
+              unsubscribeOthers(sub);
+            });
+          }
+
+          if (index === winnerIndex) {
+            subject.next(value);
+          }
       },
       error: (err) => {
-        if (!winnerFound) {
-          winnerFound = true;
-          subscriptions.forEach(s => {
-            if (s !== sub) s.unsubscribe();
+        if (winnerIndex === -1) {
+          winnerIndex = index;
+          queueMicrotask(() => {
+            unsubscribeOthers(sub);
           });
         }
-        subject.error(err);
+
+        if (index === winnerIndex) {
+          subject.error(err);
+        }
       },
       complete: () => {
-        if (!winnerFound) {
-          winnerFound = true;
-          subscriptions.forEach(s => {
-            if (s !== sub) s.unsubscribe();
-          });
-        }
-        subject.complete();
-      }
+          if (winnerIndex === -1) {
+            winnerIndex = index;
+            queueMicrotask(() => {
+              unsubscribeOthers(sub);
+            });
+          }
+
+          if (index === winnerIndex) {
+            subject.complete();
+          }
+      },
     });
+
     subscriptions.push(sub);
   });
 
