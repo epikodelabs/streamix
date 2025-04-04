ec901884b899ed08389ec225bea24d44f9c0cb43
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-04-04 11:00:50 +0300

    instead of end, count parameter is awaited in range

--- projects/libraries/streamix/src/lib/streams/range.ts
+++ projects/libraries/streamix/src/lib/streams/range.ts
@@ -1,9 +1,9 @@
 import { Stream } from '../abstractions';
 import { loop } from './loop';
 
-export function range(start: number, end: number, step: number = 1): Stream<number> {
+export function range(start: number, count: number, step: number = 1): Stream<number> {
   // Create the custom run function for the RangeStream
-  const stream = loop(start, current => current < end, current => current + step);
+  const stream = loop(start, current => current < start + count, current => current + step);
 
   stream.name = 'range';
   return stream;
