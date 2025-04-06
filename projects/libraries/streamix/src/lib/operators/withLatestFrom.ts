15853ed8b6e6421f2a47888b94779f8b8932932f
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-04-06 14:57:35 +0300

    updated withLatestFrom

--- projects/libraries/streamix/src/lib/operators/withLatestFrom.ts
+++ projects/libraries/streamix/src/lib/operators/withLatestFrom.ts
@@ -2,38 +2,38 @@ import { createMapper, createReceiver, createSubscription, Receiver, Stream, Str
 import { createSubject } from "../streams";
 
 export const withLatestFrom = (...streams: Stream<any>[]): StreamMapper => {
+  let latestValues: any[] = new Array(streams.length).fill(undefined);
+  let hasValue: boolean[] = new Array(streams.length).fill(false);
+  let otherStreamsCompleted: boolean[] = new Array(streams.length).fill(false);
+  let inputCompleted = false;
+  let allCompleted = false;
+
+  let inputSubscription: Subscription | null = null;
+  let subscriptions: Subscription[] = [];
+  const output = createSubject();
+
+  subscriptions = streams.map((stream, index) =>
+    stream.subscribe({
+      next: (value) => {
+        latestValues[index] = value;
+        hasValue[index] = true;
+      },
+      error: (err) => output.error(err),
+      complete: () => {
+        otherStreamsCompleted[index] = true;
+        checkCompletion();
+      },
+    })
+  );
+
+  const checkCompletion = () => {
+    if (inputCompleted && otherStreamsCompleted.every((c) => c) && !allCompleted) {
+      allCompleted = true;
+      output.complete();
+    }
+  };
+
   const operator = function (input: Stream): Stream {
-    const output = createSubject();
-
-    let latestValues: any[] = new Array(streams.length).fill(undefined);
-    let hasValue: boolean[] = new Array(streams.length).fill(false);
-    let otherStreamsCompleted: boolean[] = new Array(streams.length).fill(false);
-    let inputCompleted = false;
-    let allCompleted = false;
-
-    let inputSubscription: Subscription | null = null;
-    let subscriptions: Subscription[] = [];
-
-    const checkCompletion = () => {
-      if (inputCompleted && otherStreamsCompleted.every((c) => c) && !allCompleted) {
-        allCompleted = true;
-        output.complete();
-      }
-    };
-
-    subscriptions = streams.map((stream, index) =>
-      stream.subscribe({
-        next: (value) => {
-          latestValues[index] = value;
-          hasValue[index] = true;
-        },
-        error: (err) => output.error(err),
-        complete: () => {
-          otherStreamsCompleted[index] = true;
-          checkCompletion();
-        },
-      })
-    );
 
     inputSubscription = input.subscribe({
       next: (value) => {
