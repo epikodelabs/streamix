66f48874cb65216a21a604fb4bfbab61f579667a
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-05-17 07:37:13 +0300

    Update first.ts

--- projects/libraries/streamix/src/lib/operators/first.ts
+++ projects/libraries/streamix/src/lib/operators/first.ts
@@ -1,24 +1,27 @@
 import { createOperator } from "../abstractions";
 
 export const first = <T = any>(predicate?: (value: T) => boolean) =>
-  createOperator("first", (source) => {
+  createOperator<T, T>('first', (source) => {
+    const iterator = source[Symbol.asyncIterator]?.() ?? source;
     let done = false;
 
     return {
       async next(): Promise<IteratorResult<T>> {
-        if (done) return { done: true, value: undefined as any };
+        if (done) return { value: undefined, done: true };
 
-        while (true) {
-          const result = await source.next();
-          if (result.done) {
-            done = true;
-            throw new Error("No elements in sequence");
+        try {
+          for await (const value of iterator) {
+            if (!predicate || predicate(value)) {
+              done = true;
+              return { value, done: false };
+            }
           }
 
-          if (!predicate || predicate(result.value)) {
-            done = true;
-            return { value: result.value, done: false };
-          }
+          done = true;
+          throw new Error("No elements in sequence");
+        } catch (err) {
+          done = true;
+          throw err;
         }
       }
     };
