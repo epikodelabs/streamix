6ffb026bd4b26f836f56eb91f84bfe9bfe9a5199
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-05-20 13:40:33 +0300

    corrected switchMap

--- projects/libraries/streamix/src/lib/operators/switchMap.ts
+++ projects/libraries/streamix/src/lib/operators/switchMap.ts
@@ -1,41 +1,65 @@
-import { createOperator, Stream } from "../abstractions";
-import { eachValueFrom } from "../converters";
+import { eachValueFrom } from '@actioncrew/streamix';
+import { createOperator, Stream, Subscription } from "../abstractions";
+import { createSubject } from "../streams";
 
-export const switchMap = <T, R>(
-  project: (value: T, index: number) => Stream<R>
-) =>
-  createOperator('switchMap', (source) => {
-    let innerIterator: AsyncIterator<R> | null = null;
-    let innerDone = true;
-    let currentIndex = 0;
+export function switchMap<T, R>(project: (value: T, index: number) => Stream<R>) {
+  return createOperator("switchMap", (source) => {
+    const output = createSubject<R>();
 
-    return {
-      async next(): Promise<IteratorResult<R>> {
-        while (true) {
-          // If no active inner or inner completed, pull new outer value and switch
-          if (innerDone) {
-            const outerResult = await source.next();
-            if (outerResult.done) {
-              return { done: true, value: undefined };
-            }
-            innerIterator = eachValueFrom(project(outerResult.value, currentIndex++));
-            innerDone = false;
-          }
+    let currentSubscription: Subscription | null = null;
+    let inputCompleted = false;
+    let currentInnerStreamId = 0;
+    let index = 0;
 
-          if (innerIterator) {
-            const innerResult = await innerIterator.next();
-
-            if (innerResult.done) {
-              innerDone = true;
-              innerIterator = null;
-              // Loop again to get next outer value
-              continue;
-            }
-
-            // Emit inner value
-            return { done: false, value: innerResult.value };
-          }
-        }
+    const checkComplete = () => {
+      if (inputCompleted && !currentSubscription) {
+        output.complete();
       }
     };
+
+    const subscribeToInner = (innerStream: Stream<R>, streamId: number) => {
+      if (currentSubscription) {
+        currentSubscription.unsubscribe();
+        currentSubscription = null;
+      }
+
+      currentSubscription = innerStream.subscribe({
+        next: (value) => {
+          if (streamId === currentInnerStreamId) {
+            output.next(value);
+          }
+        },
+        error: (err) => {
+          if (streamId === currentInnerStreamId) {
+            output.error(err);
+          }
+        },
+        complete: () => {
+          if (streamId === currentInnerStreamId) {
+            currentSubscription = null;
+            checkComplete();
+          }
+        },
+      });
+    };
+
+    (async () => {
+      try {
+        while (true) {
+          const { value, done } = await source.next();
+          if (done) break;
+
+          const streamId = ++currentInnerStreamId;
+          const innerStream = project(value, index++);
+          subscribeToInner(innerStream, streamId);
+        }
+        inputCompleted = true;
+        checkComplete();
+      } catch (err) {
+        output.error(err);
+      }
+    })();
+
+    return eachValueFrom(output);
   });
+}
