2603c9cbec61f33921d1536e91e65786bffdb92f
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-05-28 13:02:29 +0300

    moved some operators to hooks

--- projects/libraries/streamix/src/lib/hooks/index.ts
+++ projects/libraries/streamix/src/lib/hooks/index.ts
@@ -1,8 +1,19 @@
 export * from './catchError';
+export * from './count';
 export * from './defaultIfEmpty';
 export * from './endWith';
+export * from './every';
 export * from './finalize';
+export * from './first';
+export * from './last';
+export * from './max';
+export * from './min';
 export * from './reduce';
+export * from './some';
 export * from './startWith';
 export * from './toArray';
+
+
+
+
 
