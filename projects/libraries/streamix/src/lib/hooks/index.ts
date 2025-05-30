aeef5218315fd33c6b78ed426518050268e75a3f
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-05-30 08:13:24 +0300

    operators moved back

--- projects/libraries/streamix/src/lib/hooks/index.ts
+++ projects/libraries/streamix/src/lib/hooks/index.ts
@@ -1,17 +1,6 @@
-export * from './catchError';
-export * from './count';
-export * from './defaultIfEmpty';
 export * from './endWith';
-export * from './every';
 export * from './finalize';
-export * from './first';
-export * from './last';
-export * from './max';
-export * from './min';
-export * from './reduce';
-export * from './some';
 export * from './startWith';
-export * from './toArray';
 
 
 
