0037aae6661cc14a1e49885c368df0629282748b
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-06-08 08:55:51 +0300

    Update replaySubject.ts

--- projects/libraries/streamix/src/lib/streams/replaySubject.ts
+++ projects/libraries/streamix/src/lib/streams/replaySubject.ts
@@ -85,8 +85,9 @@ export function createReplaySubject<T = any>(capacity: number = Infinity): Repla
         } catch (err: any) {
           receiver.error?.(err);
         } finally {
-          subscribers.delete(receiver);
+          if (!unsubscribing) { await buffer.detachReader(readerId); }
           receiver.complete();
+          subscribers.delete(receiver);
         }
       });
     });
