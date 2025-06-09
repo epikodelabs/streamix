ddc4840aa9f4f16cb39df8a511f42e457c747f07
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-06-09 23:33:06 +0300

    Update subscription.ts

--- projects/libraries/streamix/src/lib/abstractions/subscription.ts
+++ projects/libraries/streamix/src/lib/abstractions/subscription.ts
@@ -41,9 +41,12 @@ export const createSubscription = function <T = any>(onUnsubscribe?: () => void)
 
       const asyncLoop = async () => {
         try {
-          for await (const value of generator()) {
-            _latestValue = value;
-            receiver.next?.(value);
+          if (!_unsubscribed) {
+            for await (const value of generator()) {
+              if (_unsubscribed) break;
+              _latestValue = value;
+              receiver.next?.(value);
+            }
           }
         } catch (err: any) {
           receiver.error?.(err);
