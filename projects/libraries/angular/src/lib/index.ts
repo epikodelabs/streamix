a4410fc97786a6576010476aebede735ef14757d
oleksii.shepel@outlook.com <oleksii.shepel@outlook.com>
2025-08-13 16:57:33 +0300

    removed obsolete projects

--- projects/libraries/angular/src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of angular
- */
-export * from './asyncPipe';
-export * from './eventEmitter';
-export * from './stream.directive';
-export * from './stream.module';
-export * from './stream.pipe';
-
