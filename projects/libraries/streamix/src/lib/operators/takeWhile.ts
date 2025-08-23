753bf072bf2040ee9cf444f8a4bfc6033bd72950
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-08-23 14:19:21 +0300

    restored takeWhile

--- projects/libraries/streamix/src/lib/operators/takeWhile.ts
+++ projects/libraries/streamix/src/lib/operators/takeWhile.ts
@@ -39,6 +39,8 @@ export const takeWhile = <T = any>(
             done = true;
             return { done: true, value: undefined };
           }
+
+          return result;
         }
       },
     };
