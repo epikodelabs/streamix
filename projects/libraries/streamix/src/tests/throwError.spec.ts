4a69c5cffc5070773bf3ad48001965b0ca7bdb8f
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-09-30 17:53:40 +0300

    added test for throwError

---
+++ projects/libraries/streamix/src/tests/throwError.spec.ts
@@ -0,0 +1,34 @@
+import { from, throwError } from '@actioncrew/streamix';
+
+describe('throwError operator', () => {
+  it('should emit an error with the given message when subscribed', (done) => {
+    const stream = from([1, 2, 3]).pipe(throwError('Boom!'));
+
+    stream.subscribe({
+      next: () => {
+        fail('Expected no values to be emitted');
+      },
+      error: (err) => {
+        expect(err).toEqual(jasmine.any(Error));
+        expect(err.message).toBe('Boom!');
+        done();
+      }
+    });
+  });
+
+  it('should complete without error if the source is empty', (done) => {
+    const stream = from([]).pipe(throwError('Never thrown'));
+
+    stream.subscribe({
+      next: () => {
+        fail('Expected no values to be emitted');
+      },
+      error: (err) => {
+        fail(`Expected no error, but got: ${err}`);
+      },
+      complete: () => {
+        done();
+      }
+    });
+  });
+});
