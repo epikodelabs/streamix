67f31db6d6f492816ec54be92346a8022e3368eb
actioncrew <163549568+actioncrew@users.noreply.github.com>
2025-10-04 10:23:45 +0300

    Update startWith.ts

--- projects/libraries/streamix/src/lib/hooks/startWith.ts
+++ projects/libraries/streamix/src/lib/hooks/startWith.ts
@@ -1,4 +1,4 @@
-import { CallbackReturnType, createOperator, DONE, NEXT, Operator } from "../abstractions";
+import { createOperator, DONE, NEXT, Operator } from "../abstractions";
 
 /**
  * Creates a stream operator that prepends a specified value to the beginning of the stream.
@@ -11,7 +11,7 @@ import { CallbackReturnType, createOperator, DONE, NEXT, Operator } from "../abs
  * @param initialValue The value to be emitted as the first item in the stream.
  * @returns An `Operator` instance that can be used in a stream's `pipe` method.
  */
-export const startWith = <T = any>(initialValue: CallbackReturnType<T>) =>
+export const startWith = <T = any>(initialValue: T) =>
   createOperator<T, T>("startWith", function (this: Operator, source) {
     let emittedInitial = false;
     let completed = false;
@@ -25,7 +25,7 @@ export const startWith = <T = any>(initialValue: CallbackReturnType<T>) =>
 
           if (!emittedInitial) {
             emittedInitial = true;
-            return NEXT(await initialValue);
+            return NEXT(initialValue);
           }
 
           const result = await source.next();
