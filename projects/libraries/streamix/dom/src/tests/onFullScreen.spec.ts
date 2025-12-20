55cc1ab11b47b149dbe2750835c6303a7fd6fae0
Oleksii Shepel <oleksii.shepel@outlook.com>
2025-12-20 20:57:14 +0200

    corrections

--- projects/libraries/streamix/dom/src/tests/onFullScreen.spec.ts
+++ projects/libraries/streamix/dom/src/tests/onFullScreen.spec.ts
@@ -16,21 +16,6 @@ function fullscreenAvailable(): boolean {
   );
 }
 
-async function enterFullscreen(el: HTMLElement): Promise<boolean> {
-  try {
-    await el.requestFullscreen();
-    return document.fullscreenElement === el;
-  } catch {
-    return false;
-  }
-}
-
-async function exitFullscreen(): Promise<void> {
-  if (document.fullscreenElement) {
-    await document.exitFullscreen();
-  }
-}
-
 /* -------------------------------------------------- */
 /* Browser-only tests                                 */
 /* -------------------------------------------------- */
