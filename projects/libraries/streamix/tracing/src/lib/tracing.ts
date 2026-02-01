a1da75f17d06f53711dcbf6a12ebdb4bf91358a0
actioncrew <sepeloleksij4@gmail.com>
2026-02-01 16:39:06 +0200

    corrections

--- projects/libraries/streamix/tracing/src/lib/tracing.ts
+++ projects/libraries/streamix/tracing/src/lib/tracing.ts
@@ -4,10 +4,6 @@
  * Exports the side-effect free core APIs, and (for backwards compatibility)
  * auto-installs the Streamix runtime hooks on import.
  */
-
-export * from "./core";
-export { installTracingHooks } from "./runtime";
-
 import { installTracingHooks } from "./runtime";
 
 // Auto-install hooks when module is imported
